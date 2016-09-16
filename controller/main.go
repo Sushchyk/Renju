7dc683c4f09ecd557e25d21f586ea37fbc3b8324
Andriy Sushchyk <sushchyk12@gmail.com>
2016-09-16 22:13:57 +0300

    Add functions for work with terminal

---
+++ controller/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"os"
+	"strconv"
+)
+
+ cmd.Run()
+}
+
+func main() {
+	GoToXY(10, 20)
+	fmt.Println("This is renju")
+
+}
---
+++ view/terminal.go
@@ -0,0 +1,22 @@
+package view
+
+import (
+	"strconv"
+	"os/exec"
+	"os"
+)
+
+func goToXY(x ,y int){
+	tput("cup", strconv.Itoa(x), strconv.Itoa(y)) // an initial position
+}
+
+func clearScreen() {
+	c := exec.Command("clear")
+	c.Stdout = os.Stdout
+	c.Run()
+}
+
+func tput(args ...string) error {
+	cmd := exec.Command("tput", args...)
+	cmd.Stdout = os.Stdout
+	return
