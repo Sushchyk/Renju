5d86db735bdce5038999c4efa45950006ffd0e25
Andriy Sushchyk <sushchyk12@gmail.com>
2016-09-25 19:22:10 +0300

    Create menu

--- main.go
+++ main.go
@@ -21,45 +21,49 @@ func readKey() string{
 }
 
 func main() {
-  	var game model.Game;
-	game.Start();
-	view.StartGame();
-	currentX, currentY := config.DEFAULT_X, config.DEFAULT_Y;
-	view.MoveCursorForTurn(currentX + 1, currentY + 1, game.GetCurrentPlayer().GetSymbol());
-	gameContinue := true;
-	for (gameContinue) {
-		moveX, moveY := 0, 0;
-		action := readKey();
-		switch action {
-		case "s":
-			moveY = 1;
-		case "w":
-			moveY = -1;
-		case "d":
-			moveX = 1;
-		case "a":
-			moveX = -1;
-		case "f":
-			if(game.MakeTurn(currentX, currentY)) {
-				view.DrawTurn(game.GetCurrentPlayer().GetSymbol());
-				game.SwitchPlayer();
-				if (game.IsRow()) {
-					gameContinue = false;
-					break;
+	for true {
+		view.InMenu();
+
+		var game model.Game;
+		game.Start();
+		view.StartGame();
+		currentX, currentY := config.DEFAULT_X, config.DEFAULT_Y;
+		view.MoveCursorForTurn(currentX + 1, currentY + 1, game.GetCurrentPlayer().GetSymbol());
+		gameContinue := true;
+		for (gameContinue) {
+			moveX, moveY := 0, 0;
+			action := readKey();
+			switch action {
+			case "s":
+				moveY = 1;
+			case "w":
+				moveY = -1;
+			case "d":
+				moveX = 1;
+			case "a":
+				moveX = -1;
+			case "f":
+				if(game.MakeTurn(currentX, currentY)) {
+					view.DrawTurn(game.GetCurrentPlayer().GetSymbol());
+					game.SwitchPlayer();
+					if (game.IsRow()) {
+						gameContinue = false;
+						break;
+					}
+					currentX, currentY = game.GetFreePositonAfterTurn();
+					view.MoveCursorAfterTurn(currentX + 1, currentY + 1, game.GetCurrentPlayer().GetSymbol());
 				}
-				currentX, currentY = game.GetFreePositonAfterTurn();
-				view.MoveCursorAfterTurn(currentX + 1, currentY + 1, game.GetCurrentPlayer().GetSymbol());
+				continue;
 			}
-			continue;
-		}
-		if (action == "w" || action == "s" || action == "d" || action == "a") {
-			newX, newY := game.GetFreePosition(currentX, currentY, moveX, moveY);
-			if (newX != -1 && newY != -1) {
-				currentY = newY;
-				currentX = newX;
-				view.MoveCursorForTurn(currentX + 1, currentY + 1, game.GetCurrentPlayer().GetSymbol());
+			if (action == "w" || action == "s" || action == "d" || action == "a") {
+				newX, newY := game.GetFreePosition(currentX, currentY, moveX, moveY);
+				if (newX != -1 && newY != -1) {
+					currentY = newY;
+					currentX = newX;
+					view.MoveCursorForTurn(currentX + 1, currentY + 1, game.GetCurrentPlayer().GetSymbol());
+				}
 			}
 		}
+		view.ShowWinner(game.GetWinnerName());
 	}
-	view.ShowWinner(game.GetWinnerName());
 }
